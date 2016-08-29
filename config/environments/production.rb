82373c92acce8402fb7c56885edf162e2193570d
pehyuhan <pehyuhan@gmail.com>
2016-08-29 12:58:05 +0000

    email4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,20 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://ad-bridged.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => 'ad.bridged123@gmail.com',
+ :password             => '!234qwer',
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
