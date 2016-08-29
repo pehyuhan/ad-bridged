f6e3300f9f89165242c43cd83a91ad5e2b278bc4
pehyuhan <pehyuhan@gmail.com>
2016-08-29 12:47:11 +0000

    email2

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -1,6 +1,5 @@
 class EnquiryMailer < ApplicationMailer
-    def enquiry(post)
-byebug        
+    def enquiry(post)     
         mail(to: post.user.email, subject: 'Welcome to My Awesome Site')
     end
 end
